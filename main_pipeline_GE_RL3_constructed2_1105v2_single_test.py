20d9df3d5c2fd2527029a92dcaa08c45ec6dd5f4
xyx <82051300+axin1301@users.noreply.github.com>
2024-11-05 16:15:13 +0200

    Add files via upload

---
+++ main_pipeline_GE_RL3_constructed2_1105v2_single_test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import glob
+# from concat_all_label_image import *
+# from RoadNetwortLable_by_each_road import *
+# from convert_matlab_code import *
+# from graph_extraction_pixel import *
+# from main_pixel import *
+# from shp2txt_transform_new import *
+# from simplify_road import *
+# from transform_coord_grid import *
+# from create_shp_from_txt_json import *
+# from filter_road_by_bound import *
+# import mapcompare_GE_0001
+import sys
+import mapcompare_GE_RL3_constructed2_1105v2_single_test
+import average_statistics_GE_RL3_constructed2_1105v2
+
+if __name__ == '__main__':
+
+    mapcompare_GE_RL3_constructed2_1105v2_single_test.main()
+    # mapcompare_GE_0001.main()
+    average_statistics_GE_RL3_constructed2_1105v2.main()
